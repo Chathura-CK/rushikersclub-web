c7c590303876260d62d1df5dec6d496c2e75bbc7
W. A. C. K. Chandrasiri <chandrasiri11572@usci.ruh.ac.lk>
2025-02-08 19:50:08 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,7 @@ app.use('/api/v1/magazine', magazine);
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
